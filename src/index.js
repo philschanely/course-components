6b380879b2183b82e39ca3a91ba9d282cc4f3f14
Phil Schanely <phil.schanely@dockyard.com>
2019-08-18 20:49:39 -0400

    WIP on basic setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Brand from "./components/brand";
+import FooterBrand from "./components/brand-footer";
+import Header from "./components/header";
+// import NavMain from "./components/nav-main";
+
+export {
+  Brand,
+  FooterBrand,
+  Header,
+  // NavMain,
+};
